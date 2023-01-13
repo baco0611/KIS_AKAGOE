1a12bed342e0441dff57946b9fb808ad4b1afc4a
baco0611 <baoblink@gmail.com>
2023-01-14 04:14:11 +0700

    Handling create new comment

--- assets/js/review.js
+++ assets/js/review.js
@@ -1,6 +1,7 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+// Make swiper of comment
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
     spaceBetween: 30,
@@ -15,10 +16,11 @@ var swiper = new Swiper(".mySwiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-});
+})
 
+// Handle readmore
 var readmoreBtns = $$('.review-main .review-readmore')
-console.log(readmoreBtns)
+// console.log(readmoreBtns)
 
 readmoreBtns.forEach(readmoreBtn => {
     readmoreBtn.onclick = () => {
@@ -27,7 +29,7 @@ readmoreBtns.forEach(readmoreBtn => {
 
         openReadmore.classList.add('open')
     }
-});
+})
 
 const readmoreDivs = $$('.review-readmore-hidden')
 
@@ -44,3 +46,76 @@ for(const reviewContainer of reviewContainers)
         e.stopPropagation();
     });
 }
+
+
+// Handle close comment
+function closeComment(cmtBox, cmtCon, cmtConOff){
+    cmtBox.classList.remove('open')
+    cmtCon.innerHTML = cmtConOff
+}
+
+// Handle text according to the rating
+function restartStar()
+{
+
+    const stars = $$('.comment-rating input')
+    console.log(stars)
+    
+    stars.forEach(star => {
+        const header = $('form header')
+        const h1 = $('form h1')
+        const textArea = $('form textarea')
+        const button = $('form button')
+        star.onclick = () => {
+            console.log(star.id)
+            if(star.checked)
+            {
+                switch (star.id) {
+                    case 'rate-5':
+                        header.innerHTML = 'I just love it!!!'
+                        break;
+                    case 'rate-4':
+                        header.innerHTML = 'I just like it!!'
+                        break;
+                    case 'rate-3':
+                        header.innerHTML = 'It\'s awesome!'
+                        break;
+                    case 'rate-2':
+                        header.innerHTML = 'I don\'t like it!!'
+                        break;
+                    case 'rate-1':
+                        header.innerHTML = 'I just hate it!!!'
+                        break;
+                
+                    default:
+                        break;
+                }
+                h1.classList.add('none')
+                textArea.classList.add('open')        
+                button.classList.add('open')        
+            }
+        }    
+    })
+}
+
+// Handle add comment
+const cmtBtn = $('.cmt-btn')
+const cmtBox = $('.comment-box')
+const cmtCon = $('.comment-container')
+const cmtConOff = cmtCon.innerHTML
+
+cmtBtn.onclick = () => {
+    cmtBox.classList.add('open')
+    restartStar()
+}
+
+cmtBox.onclick = () => {
+    closeComment(cmtBox, cmtCon, cmtConOff)
+}
+
+cmtCon.onclick = e => {
+    e.stopPropagation()
+}
+
+
+
